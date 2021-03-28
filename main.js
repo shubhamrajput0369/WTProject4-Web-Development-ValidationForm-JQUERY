a13bb24461cd9958e3523209349362612f67ef53
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-03-28 22:31:22 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-  
 // Author: Shubham Bhagwansing Rajput
 // JS
 
@@ -7,4 +6,23 @@ $(document).ready(function () {
     jQuery.validator.addMethod("custom", function (value, element) {
         return /^[a-zA-Z]+$/.test(value);
     }, "* Please enter something");
-
+    $('form[id="regs"]').validate({
+        rules: {
+            fname: {
+                required: true,
+                custom: true,
+            },
+            lname: {
+                required: true,
+                custom: true,
+            },
+            user_email: {
+                required: true,
+                email: true
+            },
+            pwd: {
+                required: true,
+                minlength: 8
+            },
+        },
+   
